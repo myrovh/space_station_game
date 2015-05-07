ec5840d7160e70bba59e3cf71c7741ca4e5a580a
myrovh <myrovh@yandex.com>
2015-05-08 00:49:46 +1000

    module script collects all its slots into list

--- Assets/scripts/module.cs
+++ Assets/scripts/module.cs
@@ -1,15 +1,19 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class module : MonoBehaviour {
+public class module : MonoBehaviour
+{
+    #region Variables
+    [SerializeField]
+    public List<GameObject> resourceSlots;
+    #endregion
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
+    void Start () {
+        // Add slot_# empties to the resourceSlots list
+        Transform hardpointList = transform.Find("hardpoints");
+        foreach (Transform child in hardpointList)
+        {
+            resourceSlots.Add(child.gameObject);
+        }
 	}
 }
