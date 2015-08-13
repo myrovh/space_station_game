e2b8e9ce53d79d1cffb8526545dad93a1732699a
LiamWatts <lpwat1@student.monash.edu>
2015-08-13 21:46:35 +1000

    Fixed all links from each scene and added after action report
    After action Report inst linked to anything yet. Would be after the
    player has left the level.

---
+++ Assets/scripts/UIButtons.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class UIButtons : MonoBehaviour {
+
+    public void TransitionLevelSelect()
+    {
+        Application.LoadLevel("level_select_menu");
+    }
+
+	public void TransitionOptions()
+	{
+		Application.LoadLevel("options_menu");
+
+	}
+
+	public void TransitionPlayLevel()
+	{
+		Application.LoadLevel ("Level_1_hardpoints");
+	}
+	
+	public void TransitionMainMenu()
+	{
+		Application.LoadLevel ("start_menu");
+	}
+
+	public void TransitionControlsMenu ()
+	{
+		Application.LoadLevel ("control_menu");
+	}
+	
+	public void TransitionPreviewLevel ()
+	{
+		Application.LoadLevel ("level_preview_menu");
+	}
+}
